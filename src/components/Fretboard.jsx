30dffb93e4123d4ea9eabf9df5b9196d61543b72
CalvinGaiennie <calvingaiennie@gmail.com>
2024-12-21 14:19:10 -0600

    worked on the fretboard simulator. got frets to light up when you hover over them

--- src/components/Fretboard.jsx
+++ src/components/Fretboard.jsx
@@ -1,23 +1,41 @@
 import styles from "./Fretboard.module.css";
 
 function Fretboard() {
+  function light(id) {
+    document.getElementById(id).classList.add(styles.active);
+  }
+
+  function unLight(id) {
+    document.getElementById(id).classList.remove(styles.active);
+  }
   return (
     <div>
       <h1>Fretboard Simulator</h1>
+
       <div className={styles.fretboard}>
         {Array.from({ length: 6 }).map((_, i) => (
-          <div
-            key={`string-${i + 1}`}
-            id={`string-${i + 1}`}
-            className={styles.string}
-          >
-            {Array.from({ length: 20 }).map((_, j) => (
-              <div
-                key={`fret-${j}-string-${i + 1}`}
-                id={`fret-${j}-string-${i + 1}`}
-                className={styles.fret}
-              ></div>
-            ))}
+          <div key={`outer-string-${i + 1}`} className={styles.outerString}>
+            <div
+              key={`string-${i + 1}`}
+              id={`string-${i + 1}`}
+              className={styles.innerString}
+            >
+              {Array.from({ length: 20 }).map((_, j) => (
+                <div
+                  key={`fret-${j}-string-${i + 1}`}
+                  id={`fret-${j}-string-${i + 1}`}
+                  className={styles.fret}
+                  onMouseEnter={() => light(`fret-${j}-string-${i + 1}`)}
+                  onMouseLeave={() => unLight(`fret-${j}-string-${i + 1}`)}
+                >
+                  <div
+                    key={`innerFret-${j}-string-${i + 1}`}
+                    id={`innerFret-${j}-string-${i + 1}`}
+                    className={styles.innerFret}
+                  ></div>
+                </div>
+              ))}
+            </div>
           </div>
         ))}
       </div>
